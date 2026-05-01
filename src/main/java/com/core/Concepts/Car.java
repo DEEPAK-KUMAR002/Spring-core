ff31bfd771092c6048b8c5761eb08e69a61bb0a4
Deepak Kumar <deepakkumar35508@gmail.com>
2026-05-01 18:06:35 +0530

    first commit

---
+++ src/main/java/com/core/Concepts/Car.java
@@ -0,0 +1,7 @@
+package com.core.Concepts;
+
+public class Car {
+    public void start(){
+        System.out.println();
+    }
+}
---
+++ src/main/java/com/core/Concepts/Engine.java
@@ -0,0 +1,4 @@
+package com.core.Concepts;
+
+public class Engine {
+}
